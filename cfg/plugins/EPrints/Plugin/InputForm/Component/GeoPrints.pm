ad684dc93cf1e3e4cc8887a588da9ac1aeef68bc
Marcus Ramsden <cuscus1986@gmail.com>
2010-02-26 09:49:57 +0000

    Initial project commit.

---
+++ cfg/plugins/EPrints/Plugin/InputForm/Component/GeoPrints.pm
@@ -0,0 +1,49 @@
+package EPrints::Plugin::InputForm::Component::GeoPrints;
+
+use EPrints::Plugin::InputForm::Component;
+
+@ISA = ( 'EPrints::Plugin::InputForm::Component' );
+
+use strict;
+
+sub new
+{
+	my( $class, %opts ) = @_;
+
+	my $self = $class->SUPER::new( %opts );
+
+	$self->{name} = "GeoPrints";
+	$self->{visible} = "all";
+
+	return $self;
+}
+
+sub render_content
+{
+	my( $self ) = @_;
+
+	my $repository = $self->{repository};
+	my $xml = $repository->{xml};
+	my $eprint = $self->{eprint};
+
+	my $frag = $xml->create_document_frag;
+
+	$frag->appendChild( $self->html_phrase( 'description' ) );
+
+	return $frag;
+}
+
+sub render_help
+{
+	my( $self, $surround ) = @_;
+
+	return $self->html_phrase( 'help' );
+}
+
+sub render_title
+{
+	my( $self, $surround )  = @_;
+
+	return $self->html_phrase( 'title' );
+}
+1;
