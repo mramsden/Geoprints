42385e7249be9da446b6c6633877f5a04ec3cb03
Marcus Ramsden <cuscus1986@gmail.com>
2010-02-27 10:47:52 +0000

    GeoPrints now uses the Yahoo Placemaker API and successfully takes a document and converts it to a list of places.

--- cfg/plugins/EPrints/Plugin/InputForm/Component/GeoPrints.pm
+++ cfg/plugins/EPrints/Plugin/InputForm/Component/GeoPrints.pm
@@ -27,11 +27,22 @@ sub render_content
 	my( $self ) = @_;
 
 	my $repository = $self->{repository};
-	my $eprint = $self->{eprint};
+	my $eprint = $self->{dataobj};
 
 	my $frag = $repository->xml->create_document_fragment;
 
-	$frag->appendChild( $self->html_phrase( 'description' ) );
+	my @documents = $eprint->get_all_documents;
+
+	if( @documents )
+	{
+		$frag->appendChild( $self->_render_interface );
+	}
+	else
+	{
+		my $no_documents = $repository->xml->create_element( 'p' );
+		$no_documents->appendChild( $self->html_phrase( 'no_documents' ) );
+		$frag->appendChild( $no_documents );
+	}
 
 	return $frag;
 }
@@ -48,5 +59,53 @@ sub render_title
 	my( $self, $surround )  = @_;
 
 	return $self->html_phrase( 'title' );
+}
+
+sub _render_interface
+{
+	my( $self ) = @_;
+
+	my $repository = $self->{repository};
+	my $eprint = $self->{dataobj};
+	
+	my $frag = $repository->xml->create_document_fragment;
+
+	my $table = $repository->xml->create_element( 'table' );
+	my $thead = $repository->xml->create_element( 'thead' );
+	my $tbody = $repository->xml->create_element( 'tbody' );
+
+	my $thead_row = $repository->xml->create_element( 'tr' );
+	my $thead_row_cell = $repository->xml->create_element( 'td' );
+	$thead_row_cell->appendChild( $self->html_phrase( 'filename' ) );
+	$thead_row->appendChild( $thead_row_cell );
+	$thead->appendChild( $thead_row );
+
+	my( $tbody_row, $td );
+
+	my @documents = $eprint->get_all_documents;
+
+	foreach my $document ( @documents )
+	{
+		$tbody_row = $repository->xml->create_element( 'tr' );
+
+		$td = $repository->xml->create_element( 'td' );
+		$td->appendChild( $repository->xml->create_text_node( $document->get_main ) );
+		$tbody_row->appendChild( $td );
+
+		my $geoprint_url = $repository->get_conf( 'perl_url' ).'/users/geoprints?eprintid='.$eprint->get_id.'&documentid='.$document->get_id;
+		my $link = $repository->xml->create_element( 'a', href => $geoprint_url );
+		$link->appendChild( $repository->xml->create_text_node( 'geoprint' ) );
+		$td = $repository->xml->create_element( 'td' );
+		$td->appendChild( $link );
+		$tbody_row->appendChild( $td );
+
+		$tbody->appendChild( $tbody_row );
+	}
+
+	$table->appendChild( $thead );
+	$table->appendChild( $tbody );
+	$frag->appendChild( $table );
+
+	return $frag;
 }
 1;
